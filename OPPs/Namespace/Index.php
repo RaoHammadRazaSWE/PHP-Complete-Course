70862cbd8ef8e4175806ddec2554ecebca1a286f
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-05-14 13:17:45 +0500

    Create Index.php

---
+++ OPPs/Namespace/Index.php
@@ -0,0 +1,20 @@
+<?php 
+    require 'products.php';
+    require 'testing.php';
+
+function wow()
+{
+    echo "Wow from Products files <br>";
+}
+    // $obj = new test\product();
+    
+    $obj = new pro\product();
+    
+    // $obj = new pro\product();
+
+    // wow();
+    
+    pro\wow();
+
+
+?>
