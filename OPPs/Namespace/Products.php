ae253d97e1342794aefd945ce8fa820f001a236e
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-05-14 13:18:27 +0500

    Create Products.php

---
+++ OPPs/Namespace/Products.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace pro;
+    class product
+    {
+        public function __construct()
+        {
+            // echo "This is Product Class<br>";
+
+            $test  = new \test\product;
+        }
+    }
+
+function wow(){
+    echo "Wow from Products files .<br>";
+}
